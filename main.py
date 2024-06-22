bd6cdbd1fef85a9ef943933d04331bbe129bde6b
thehalfbloodguy <bogdan.lavriy@gmail.com>
2024-06-23 01:19:39 +0300

    Sort notes by page number before export to Notion

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ if __name__ == "__main__":
         DATABASE_ID = config['database_id']
         PB_NOTES_DIR = config['pb_notes_dir']
 
-    pb_book_names = os.listdir(PB_NOTES_DIR)
-    if '.sync_file.json' in pb_book_names:
+    pb_notes_pagenames = os.listdir(PB_NOTES_DIR)
+    if '.sync_file.json' in pb_notes_pagenames:
         synced_notes = json.load(open(f"{PB_NOTES_DIR}/.sync_file.json"))
-        pb_book_names.remove('.sync_file.json')
+        pb_notes_pagenames.remove('.sync_file.json')
     else:
         synced_notes = dict()
 
@@ -31,26 +31,28 @@ if __name__ == "__main__":
     response = db.query(HEADERS)
     results = response.json()['results']
     db_page_map = {res['properties']['Name']['title'][0]['plain_text']: res['id'] for res in results}
-    for pb_book_name in pb_book_names:
-        book_page = NotesPage(f'{PB_NOTES_DIR}/{pb_book_name}')
+    for pb_notes_pagename in pb_notes_pagenames:
+        book_name = os.path.splitext(pb_notes_pagename)[0]
+        book_page = NotesPage(f'{PB_NOTES_DIR}/{pb_notes_pagename}')
         book_page.parse()
-        if pb_book_name in db_page_map:
-            page = Page(db_page_map[pb_book_name])
+        if book_name in db_page_map:
+            page = Page(db_page_map[book_name])
             response = page.get_children(HEADERS)
             page_blocks = response.json()['results']
-            unsynced_notes = list(filter(lambda x: x.id not in synced_notes[pb_book_name], book_page.notes))
+            unsynced_notes = list(filter(lambda x: x.id not in synced_notes[pb_notes_pagename], book_page.notes))
             for note in unsynced_notes:
                 if len(note.text) < MAX_NOTE_LENGTH: # add splitting if it is >= MAX_NOTE_LENGTH
                     page.add_child(note, HEADERS)
-                    synced_notes[pb_book_name].append(note.id)
+                    synced_notes[pb_notes_pagename].append(note.id)
         else:
             filtered_notes = list()
             for note in book_page.notes:
                 if len(note.text) < MAX_NOTE_LENGTH:
                     filtered_notes.append(note)
+            filtered_notes = sorted(filtered_notes, key=lambda x: int(x.page_no))
             if len(filtered_notes) > MAX_BATCH_SIZE:
                 filtered_notes = filtered_notes[:MAX_BATCH_SIZE]
-            response = db.createPage(pb_book_name, filtered_notes, HEADERS)
+            response = db.createPage(book_name, filtered_notes, HEADERS)
             if response.status_code == 200:
-                synced_notes[pb_book_name] = [note.id for note in book_page.notes]
+                synced_notes[pb_notes_pagename] = [note.id for note in book_page.notes]
     json.dump(synced_notes, open(f"{PB_NOTES_DIR}/.sync_file.json", "w"))
